1a37f52b8a0a7b59a48fcd827637a972bc6676db
Mundurujoana <mundujoana@gmail.com>
2023-10-16 16:53:04 +0200

    fetches customer names from a MySQL database and displays additional information for a selected customer, including their total amount, amount owed, and missed payments.

--- src/CustomerList.js
+++ src/CustomerList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './customerList.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const CustomerList = () => {
   const [customerNames, setCustomerNames] = useState([]);
@@ -11,7 +12,7 @@ const CustomerList = () => {
     const fetchCustomerNames = async () => {
       try {
         const response = await fetch('/api/customers');
-        const data = await response?.json();
+        const data = await response.json();
         setCustomerNames(data.map((customer) => customer.customer_name));
       } catch (error) {
         console.error('Error fetching customer names:', error);
@@ -27,17 +28,15 @@ const CustomerList = () => {
     setIsSubmitted(false);
 
     try {
-      // Fetch the cust_id based on the selected customer's name
       const response = await fetch(`/api/customers?name=${customerName}`);
-      const data = await response?.json();
+      const data = await response.json();
 
       if (data && data.length > 0) {
         const custId = data.find((customer) => customer.customer_name === customerName)?.cust_id;
 
         if (custId) {
-          // Fetch the credit score information based on the cust_id
           const creditScoreResponse = await fetch(`/api/customers/${custId}/creditScore`);
-          const creditScoreData = await creditScoreResponse?.json();
+          const creditScoreData = await creditScoreResponse.json();
           setCustomerInfo(creditScoreData);
         } else {
           console.error('No matching customer found for the selected name:', customerName);
@@ -57,10 +56,10 @@ const CustomerList = () => {
 
   return (
     <div className="customer-list-container">
-      <h2>Customer Names</h2>
+      <h2> Health Facilities </h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Select a customer:</label>
+          <label>Select a facility:</label>
           <select value={selectedCustomer} onChange={handleSelectChange}>
             <option value="">Select a customer</option>
             {customerNames.map((name, index) => (
@@ -77,13 +76,32 @@ const CustomerList = () => {
 
       {selectedCustomer && isSubmitted && customerInfo && (
         <div className="customer-info">
-          <h2>Customer Information</h2>
-          <p>Customer Name: {selectedCustomer}</p>
+          <h2> {selectedCustomer} Information</h2>
+          <p>Facility Name: {selectedCustomer}</p>
           <p>Total amount: {customerInfo.total_amount}</p>
+          <p>Credit category: {customerInfo.credit_category}</p>
           <p>Amount owed: {customerInfo.amount_owed}</p>
           <p>Missed payments: {customerInfo.missed_payments}</p>
         </div>
       )}
+
+      {selectedCustomer && isSubmitted && customerInfo && (
+        <div className="customer-info">
+          <h2> {selectedCustomer} Creditscore visualisation </h2>
+          <BarChart width={600} height={400} data={[customerInfo]}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total_amount" fill="#8884d8" name="Total Amount" />
+            <Bar dataKey="amount_owed" fill="#82ca9d" name="Amount Owed" />
+            <Bar dataKey="missed_payments" fill="#ffc658" name="Missed Payments" />
+          </BarChart>
+        </div>
+      )}
+
+      
     </div>
   );
 };
