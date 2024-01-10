b9a1252b827130ce4b08b9f20c0d85e80dc21b73
bizip <bizip04@gmail.com>
2024-01-10 12:21:50 +0200

    Add markers from csv

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,21 +1,38 @@
-import { MapContainer, TileLayer, Polygon, Marker, Popup, Polyline } from 'react-leaflet';
-import "leaflet/dist/leaflet.css";
-import { polyData } from "./data/polygon";
-import { lineData } from "./data/line";
-import classes from "./map.module.css";
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import Papa from 'papaparse';
+import Axios from 'axios';
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+
+import { FaUpload } from 'react-icons/fa';
+import classes from './map.module.css';
 
 const MapComponent = () => {
   const center = [-1.9505012, 30.054957];
-  // const center = [39.8283, -98.5795];
   const position = [-1.954680759129028, 30.103632436377925];
+  const [csvData, setCsvData] = useState([]);
 
-  const customMarker = new L.icon({
-    iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Axios.get('http://localhost:5000/api/csv');
+        setCsvData(response.data);
+      } catch (error) {
+        console.error('Error fetching CSV:', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+  const customMarker = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png',
     iconSize: [25, 41],
     iconAnchor: [10, 41],
-    popupAnchor: [2, -40]
+    popupAnchor: [2, -40],
   });
+
   return (
     <div className={classes.map_container}>
       <MapContainer center={center} zoom={8} style={{ height: '100vh', width: '100%' }}>
@@ -23,55 +40,25 @@ const MapComponent = () => {
           url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=F7824iHaxmUF3BBsvP59"
           attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         />
-        {lineData.features.map((district, index) => {
-          if (district.geometry.type === "LineString") {
-            const coordinates = district.geometry.coordinates.map(item => [item[1], item[0]]);
-            
-            return (
-              <Polyline
-                key={index}
-                pathOptions={{
-                  color: "#FD8D2C",
-                  weight: 2,
-                  opacity: 1,
-                  dashArray: 3,
-                }}
-                positions={coordinates}
-                eventHandlers={{
-                  mouseover: (e) => {
-                    const layer = e.target;
-                    layer.setStyle({
-                      weight: 5,
-                      dashArray: "",
-                      color: "#666",
-                    });
-                  },
-                  mouseout: (e) => {
-                    const layer = e.target;
-                    layer.setStyle({
-                      weight: 2,
-                      dashArray: 3,
-                      color: "#FD8D2C",
-                    });
-                  },
-                  click: (e) => {
-                    const layer = e.target;
-                    console.log("Clicked on:", layer);
-                  }
-                }}
-              />
-            );
-          }
-          
-          // Handle other geometry types if needed
-        
-          return null;
+        {csvData.length>0 && csvData.map(item=>{
+          return <Marker
+          position={[parseFloat(item.Latitude), parseFloat(item.Longitude)]}
+          icon={L.icon({
+            iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png',
+            iconSize: [25, 41],
+            iconAnchor: [10, 41],
+            popupAnchor: [2, -40],
+          })}
+        >
+          <Popup>
+            {/* Customize the popup content based on your CSV data */}
+            <div>
+              <p>Other information here</p>
+            </div>
+          </Popup>
+        </Marker>
         })}
-        <Marker position={position}>
-        <Popup icon={customMarker}>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+    
       </MapContainer>
     </div>
   );
