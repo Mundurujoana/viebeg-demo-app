f12a11c68eb2151c0a6f2a1da704704dfcdb5a16
bizip <bizip04@gmail.com>
2024-01-17 13:53:44 +0200

    Add a deploed link

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,19 +1,59 @@
-import { MapContainer, TileLayer, Polygon, GeoJSON } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Polygon, GeoJSON, Tooltip, Popup  } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
+import Axios from 'axios';
 import { polyData } from "./data/polygon";
 import classes from "./map.module.css";
 
 const MapComponent = () => {
+  const [tooltipContent, setTooltipContent] = useState('');
+  const [districtData, setDistrictData] = useState('');
+  const [isOnHover, setIsOnHover] = useState(false);
+  const [isOnClick, setIsOnClick] = useState(false);
   const center = [-1.9505012, 30.054957];
   // const center = [39.8283, -98.5795];
 
-  const onDistrictClick = (event) => {
+  const onDistrictClick = async(event) => {
     const properties = event.target.feature.properties;
-    // const districtName = properties ? properties.Name : 'Unknown District';
-    console.log(properties);
+    const districtName = properties ? properties.Name : 'Unknown District';
+    setIsOnClick(prev=>!prev);
+    console.log(districtName);
+    try {
+      const response = await Axios.get('https://viebeg-server-xfg4.onrender.com/api/data');
+      console.log(response.data);
+      setDistrictData(response.data.districtData1)
+      // setCsvData(response.data);
+    } catch (error) {
+      console.error('Error fetching CSV:', error);
+    }
     // setDistrictName(districtName);
   };
 
+  const onDistrictMouseOver = (event) => {
+    setIsOnHover(true);
+    event.target.setStyle({
+      fillColor: "#FD8D2C",
+      fillOpacity: 0.6,
+      weight: 2,
+      opacity: 1,
+      dashArray: 3,
+      color: "white",
+    });
+    setTooltipContent(event.target.feature.properties.Name || 'Unknown District');
+  };
+
+  const onDistrictMouseOut = (event) => {
+    setIsOnHover(false);
+    event.target.setStyle({
+      fillColor: "#FD8D2C",
+      fillOpacity: 0.8,
+      weight: 2,
+      opacity: 1,
+      dashArray: 3,
+      color: "white",
+    });
+    setTooltipContent('');
+  };
   return (
     <div className={classes.map_container}>
       <MapContainer center={center} zoom={8.5} style={{ height: '100vh', width: '100%' }}>
@@ -25,7 +65,7 @@ const MapComponent = () => {
         data={polyData}
         style={() => ({
           fillColor: "#FD8D2C",
-          fillOpacity: 0.7,
+          fillOpacity: 0.8,
           weight: 2,
           opacity: 1,
           dashArray: 3,
@@ -34,9 +74,29 @@ const MapComponent = () => {
         onEachFeature={(feature, layer) => {
           layer.on({
             click: onDistrictClick,
+            mouseover: onDistrictMouseOver,
+            mouseout: onDistrictMouseOut,
           });
         }}
-      />
+        >
+        <Tooltip>{tooltipContent}</Tooltip>
+
+        <Popup>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Province</td>
+                    <td>districtData.Province</td>
+                  </tr>
+                  <tr>
+                  <td>Sectors</td>
+                  <td>{districtData.sectors}</td>
+                </tr>
+                </tbody>
+              </table>
+            </Popup>
+          )}
+        </GeoJSON>
       </MapContainer>
     </div>
   );
